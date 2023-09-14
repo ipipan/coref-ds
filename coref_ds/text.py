8e23cf03e97145df275f1c4886eebe58026dad0e
Karol Saputa <karol.saputa@gmail.com>
2023-09-14 20:39:26 +0200

    feat: paragraph and sentence subtexts

--- coref_ds/text.py
+++ coref_ds/text.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import copy
 
 
 @dataclass
@@ -74,3 +75,48 @@ class Text:
                 heads[mention_span] = self.indices_to_mentions[mention_span].head
 
         return heads
+
+    @staticmethod
+    def get_subtexts(text, split_key='last_in_sent'):
+        subtexts = []
+        curr_subtext = copy.deepcopy(text)
+        curr_segments = []
+        curr_segments_meta = []
+        curr_start_ind = 0
+
+        name_dict = {'last_in_sent': 's', 'last_in_par': 'p'}
+
+        for ind, segment in enumerate(text.segments):
+            curr_segments.append(segment)
+            curr_segments_meta.append(text.segments_meta[ind])
+
+            if getattr(text.segments_meta[ind], split_key, None):
+                curr_subtext.segments = curr_segments
+                curr_subtext.segments_meta = curr_segments_meta
+                curr_subtext.text_id += f"_{name_dict[split_key]}_{len(subtexts)}"
+                Text.trim_indexes_after_split(curr_subtext, curr_start_ind, ind)
+                subtexts.append(curr_subtext)
+                curr_subtext = copy.deepcopy(text)
+                curr_segments = []
+                curr_segments_meta = []
+                curr_start_ind = ind + 1
+
+        return subtexts
+
+    @staticmethod
+    def trim_indexes_after_split(text, text_start_ind, text_end_ind):
+        text_range = range(text_start_ind, text_end_ind)
+        new_clusters = []
+        for cluster_ind, cluster in enumerate(text.clusters):
+            new_spans = []
+            for span_start, span_end in cluster:
+                if span_start not in text_range or span_end not in text_range:
+                    continue
+                else:
+                    new_span = span_start - text_start_ind, span_end - text_start_ind
+                    new_spans.append(new_span)
+
+            if new_spans:
+                new_clusters.append(new_spans)
+        text.clusters = new_clusters
+        return text
